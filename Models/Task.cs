e042569ba1a4a36e9660f0e903026e1b5bf4767c
Михаил Казаков <mishakazakov82@gmail.com>
2026-03-23 20:04:54 +0500

    Разработанные модели данных. Казаков

---
+++ Models/Task.cs
@@ -0,0 +1,29 @@
+﻿namespace API_Kazakov.Models
+{
+    /// <summary>
+    /// Класс Задачи
+    /// </summary>
+    public class Task
+    {
+        /// <summary>
+        /// Код задачи
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// Наименование задачи
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Приоритет задачи
+        /// </summary>
+        public string Property { get; set;}
+        /// <summary>
+        /// Дата выполнения задачи
+        /// </summary>
+        public DateTime DataExcute { get; set;}
+        /// <summary>
+        /// Выполнение задачи
+        /// </summary>
+        public bool Done { get; set;}
+    }
+}
