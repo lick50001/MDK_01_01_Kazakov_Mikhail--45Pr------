db0e9a151aea8bd8e81e208e6a677f1b968ee3af
Михаил Казаков <mishakazakov82@gmail.com>
2026-03-23 20:14:55 +0500

    Разработанный контекст для загрузки данных. Казаков

---
+++ Contexts/TaskContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API_Kazakov.Contexts
+{
+    public class TaskContext : DbContext
+    {
+        public DbSet<Task> Task {  get; set; }
+        public TaskContext()
+        {
+            Database.EnsureCreated();
+            Task.Load();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql("server=localhost;uid=root;pwd=;database=TaskManager", new MySqlServerVersion(new Version(8, 0, 11)));
+        }
+    }
+}
