037b76797791aea69bc3d25806b3ee098cb9e93c
Михаил Казаков <mishakazakov82@gmail.com>
2026-03-23 19:55:43 +0500

    Настройка проекта, подключение swagger. Казаков

--- Program.cs
+++ Program.cs
@@ -1,15 +1,35 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// --- 1. РЕГИСТРАЦИЯ СЕРВИСОВ (builder.Services) ---
 builder.Services.AddRazorPages();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(option =>
+{
+    option.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Пробник"
+    });
+});
 
+// Собираем приложение (этот вызов должен быть ОДИН раз)
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+// --- 2. НАСТРОЙКА КОНФИГУРАЦИИ (app.Use...) ---
+
+// Swagger должен быть в начале, чтобы он работал
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Пробник");
+    });
+}
+else
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -17,9 +37,11 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
 app.UseAuthorization();
 
+// Настройка путей для контроллеров и страниц
+app.MapControllers();
 app.MapRazorPages();
 
+// Запуск приложения (этот вызов должен быть ОДИН раз в самом конце)
 app.Run();
