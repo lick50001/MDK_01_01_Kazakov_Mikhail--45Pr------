d289b1f8822e316e50fe68843d9381263cc68114
Михаил Казаков <mishakazakov82@gmail.com>
2026-03-24 08:24:04 +0500

    Создание модели и контекста пользователя. Казаков

---
+++ Contexts/UsersContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API_Kazakov.Contexts
+{
+    public class UsersContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public UsersContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(
+                "server=localhost;uid=root;pwd=;database=TaskManager",
+                new MySqlServerVersion(new Version(8, 0, 11)));
+        }
+    }
+}
---
+++ Models/Users.cs
@@ -0,0 +1,9 @@
+﻿namespace API_Kazakov.Models
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
